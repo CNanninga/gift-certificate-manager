2e35b582c86702946690bd4eda54491155652201
Chris Nanninga <tarsusc81@gmail.com>
2026-06-11 16:25:04 -0500

    Add server action allowed origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const appHost = process.env.APP_ORIGIN
+  ? new URL(process.env.APP_ORIGIN).host
+  : "";
+
 const nextConfig: NextConfig = {
   logging: { fetches: { fullUrl: true } },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [appHost, "localhost:3000"].filter(Boolean),
+    },
+  },
 };
 
 export default nextConfig;
